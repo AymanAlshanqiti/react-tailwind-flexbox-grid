615307d6415808e5dd7d8d92850b4251eba5f794
AymanAlshanqiti <ayman.alshanqiti@gmail.com>
2022-06-05 16:41:39 +0300

    enh: flex wrap enhancements.

--- src/components/flexbox/FlexWrap.jsx
+++ src/components/flexbox/FlexWrap.jsx
@@ -1,38 +1,48 @@
-import React from 'react';
+const shapeClasses =
+	'bg-indigo-600 h-14 w-3/12 flex justify-center items-center rounded-lg text-white';
+const itemClasses = 'w-full border p-3 rounded-lg gap-3 text-center';
+const tagClasses = 'bg-slate-100 px-2 rounded-md';
+const tagContainerClasses = 'flex gap-2 text-indigo-500';
 
 export default function FlexWrap() {
 	return (
 		<div className="flex flex-wrap gap-3 border-2 border-indigo-300 h-full p-3 rounded-lg">
 			<h1 className="w-full text-indigo-500 font-bold text-2xl">Flex Wrap</h1>
 
-			<pre className="flex gap-2 text-indigo-500">
+			<pre className={tagContainerClasses}>
 				1.
-				<span className="bg-slate-100 px-2 rounded-md">flex-nowrap</span>
+				<span className={tagClasses}>flex-nowrap</span>
 			</pre>
-			<div className="flex flex-nowrap w-full border p-3 rounded-lg gap-3 text-center overflow-x-scroll">
-				<div className="bg-indigo-600 p-5 rounded-lg text-white w-4/12">01</div>
-				<div className="bg-indigo-600 p-5 rounded-lg text-white w-4/12">02</div>
-				<div className="bg-indigo-600 p-5 rounded-lg text-white w-4/12">03</div>
+			<div className={`flex flex-nowrap ${itemClasses}`}>
+				<div className={shapeClasses}>01</div>
+				<div className={shapeClasses}>02</div>
+				<div className={shapeClasses}>03</div>
+				<div className={shapeClasses}>04</div>
+				<div className={shapeClasses}>05</div>
 			</div>
 
-			<pre className="flex gap-2 text-indigo-500">
+			<pre className={tagContainerClasses}>
 				2.
-				<span className="bg-slate-100 px-2 rounded-md">flex-wrap</span>
+				<span className={tagClasses}>flex-wrap</span>
 			</pre>
-			<div className="flex flex-wrap w-full border p-3 rounded-lg gap-3 text-center overflow-x-scroll">
-				<div className="bg-indigo-600 p-5 rounded-lg text-white w-4/12">01</div>
-				<div className="bg-indigo-600 p-5 rounded-lg text-white w-4/12">02</div>
-				<div className="bg-indigo-600 p-5 rounded-lg text-white w-4/12">03</div>
+			<div className={`flex flex-wrap ${itemClasses}`}>
+				<div className={shapeClasses}>01</div>
+				<div className={shapeClasses}>02</div>
+				<div className={shapeClasses}>03</div>
+				<div className={shapeClasses}>04</div>
+				<div className={shapeClasses}>05</div>
 			</div>
 
-			<pre className="flex gap-2 text-indigo-500">
+			<pre className={tagContainerClasses}>
 				3.
-				<span className="bg-slate-100 px-2 rounded-md">flex-wrap-reverse</span>
+				<span className={tagClasses}>flex-wrap-reverse</span>
 			</pre>
-			<div className="flex flex-wrap-reverse w-full border p-3 rounded-lg gap-3 text-center overflow-x-scroll">
-				<div className="bg-indigo-600 p-5 rounded-lg text-white w-4/12">01</div>
-				<div className="bg-indigo-600 p-5 rounded-lg text-white w-4/12">02</div>
-				<div className="bg-indigo-600 p-5 rounded-lg text-white w-4/12">03</div>
+			<div className={`flex flex-wrap-reverse ${itemClasses}`}>
+				<div className={shapeClasses}>01</div>
+				<div className={shapeClasses}>02</div>
+				<div className={shapeClasses}>03</div>
+				<div className={shapeClasses}>04</div>
+				<div className={shapeClasses}>05</div>
 			</div>
 		</div>
 	);
