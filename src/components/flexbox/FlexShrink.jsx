37b5b55e43d388b18967b5fd398086748c699341
AymanAlshanqiti <ayman.alshanqiti@gmail.com>
2022-06-05 17:16:20 +0300

    enh: flex shrink enhancements.

--- src/components/flexbox/FlexShrink.jsx
+++ src/components/flexbox/FlexShrink.jsx
@@ -1,37 +1,41 @@
-import React from 'react';
+const shapeClasses = 'h-14 justify-center items-center rounded-lg text-white';
+const itemClasses =
+	'flex flex-row w-full border p-3 rounded-lg gap-3 text-center';
+const tagClasses = 'bg-slate-100 px-2 rounded-md';
+const tagContainerClasses = 'flex gap-2 text-indigo-500';
 
 export default function FlexShrink() {
 	return (
 		<div className="flex flex-row flex-wrap gap-3 border-2 border-indigo-300 h-full p-3 rounded-lg">
 			<h1 className="w-full text-indigo-500 font-bold text-2xl">Flex Shrink</h1>
-			<pre className="flex gap-2 text-indigo-500">
+			<pre className={tagContainerClasses}>
 				1.
-				<span className="bg-slate-100 px-2 rounded-md">shrink</span>
+				<span className={tagClasses}>shrink</span>
 			</pre>
-			<div className="flex flex-row w-full border p-3 rounded-lg gap-3 text-center">
-				<div className="bg-indigo-600 rounded-lg text-white w-14 h-14 flex flex-none items-center justify-center">
+			<div className={itemClasses}>
+				<div className={`${shapeClasses} bg-indigo-300 w-14 flex flex-none`}>
 					01
 				</div>
-				<div className="bg-indigo-600 rounded-lg text-white w-64 h-14 flex shrink items-center justify-center">
+				<div className={`${shapeClasses} bg-indigo-600 w-64 flex shrink`}>
 					02
 				</div>
-				<div className="bg-indigo-600 rounded-lg text-white w-14 h-14 flex flex-none items-center justify-center">
+				<div className={`${shapeClasses} bg-indigo-300 w-32 flex flex-none`}>
 					03
 				</div>
 			</div>
 
-			<pre className="flex gap-2 text-indigo-500">
+			<pre className={tagContainerClasses}>
 				2.
-				<span className="bg-slate-100 px-2 rounded-md">shrink-0</span>
+				<span className={tagClasses}>shrink-0</span>
 			</pre>
-			<div className="flex flex-row w-full border p-3 rounded-lg gap-3 text-center">
-				<div className="bg-indigo-600 rounded-lg text-white w-14 h-14 flex flex-none items-center justify-center">
+			<div className={itemClasses}>
+				<div className={`${shapeClasses} bg-indigo-300 w-14 flex flex-none`}>
 					01
 				</div>
-				<div className="bg-indigo-600 rounded-lg text-white w-32 h-14 flex shrink-0 items-center justify-center">
+				<div className={`${shapeClasses} bg-indigo-600 w-28 flex shrink-0`}>
 					02
 				</div>
-				<div className="bg-indigo-600 rounded-lg text-white w-14 h-14 flex flex-none items-center justify-center">
+				<div className={`${shapeClasses} bg-indigo-300 w-20 flex flex-none`}>
 					03
 				</div>
 			</div>
