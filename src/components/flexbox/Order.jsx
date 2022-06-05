a64066116ee6d1d1b6161845ecac74c89e1a12e6
AymanAlshanqiti <ayman.alshanqiti@gmail.com>
2022-06-05 17:23:35 +0300

    enh: flex order enhancements.

--- src/components/flexbox/Order.jsx
+++ src/components/flexbox/Order.jsx
@@ -1,55 +1,46 @@
-import React from 'react';
+const shapeClasses =
+	'w-14 h-14 justify-center items-center rounded-lg text-white';
+const itemClasses =
+	'flex flex-row w-full border p-3 rounded-lg gap-3 text-center';
+const tagClasses = 'bg-slate-100 px-2 rounded-md';
+const tagContainerClasses = 'flex gap-2 text-indigo-500';
 
 export default function Order() {
 	return (
 		<div className="flex flex-row flex-wrap gap-3 border-2 border-indigo-300 h-full p-3 rounded-lg">
 			<h1 className="w-full text-indigo-500 font-bold text-2xl">Order</h1>
-			<pre className="flex gap-2 text-indigo-500">
+			<pre className={tagContainerClasses}>
 				1.
-				<span className="bg-slate-100 px-2 rounded-md">order-last</span>
+				<span className={tagClasses}>order-last</span>
 			</pre>
-			<div className="flex flex-row w-full border p-3 rounded-lg gap-3 text-center">
-				<div className="bg-indigo-600 rounded-lg text-white w-14 h-14 flex flex-none items-center justify-center">
+			<div className={itemClasses}>
+				<div className={`${shapeClasses} bg-indigo-600 flex order-last`}>
 					01
 				</div>
-				<div className="bg-indigo-600 rounded-lg text-white w-14 h-14 order-last flex shrink items-center justify-center">
-					02
-				</div>
-				<div className="bg-indigo-600 rounded-lg text-white w-14 h-14 flex flex-none items-center justify-center">
-					03
-				</div>
+				<div className={`${shapeClasses} bg-indigo-300 flex`}>02</div>
+				<div className={`${shapeClasses} bg-indigo-300 flex`}>03</div>
 			</div>
 
-			<pre className="flex gap-2 text-indigo-500">
+			<pre className={tagContainerClasses}>
 				2.
-				<span className="bg-slate-100 px-2 rounded-md">order-first</span>
+				<span className={tagClasses}>order-first</span>
 			</pre>
-			<div className="flex flex-row w-full border p-3 rounded-lg gap-3 text-center">
-				<div className="bg-indigo-600 rounded-lg text-white w-14 h-14 flex items-center justify-center">
-					01
-				</div>
-				<div className="bg-indigo-600 rounded-lg text-white w-14 h-14 flex items-center justify-center">
-					02
-				</div>
-				<div className="bg-indigo-600 rounded-lg text-white w-14 h-14 flex order-first items-center justify-center">
+			<div className={itemClasses}>
+				<div className={`${shapeClasses} bg-indigo-300 flex`}>01</div>
+				<div className={`${shapeClasses} bg-indigo-300 flex`}>02</div>
+				<div className={`${shapeClasses} bg-indigo-600 flex order-first`}>
 					03
 				</div>
 			</div>
 
-			<pre className="flex gap-2 text-indigo-500">
+			<pre className={tagContainerClasses}>
 				3.
-				<span className="bg-slate-100 px-2 rounded-md">order-[number]</span>
+				<span className={tagClasses}>order-[number]</span>
 			</pre>
-			<div className="flex flex-row w-full border p-3 rounded-lg gap-3 text-center">
-				<div className="bg-indigo-600 rounded-lg text-white w-14 h-14 order-3 flex flex-none items-center justify-center">
-					01
-				</div>
-				<div className="bg-indigo-600 rounded-lg text-white w-14 h-14 order-1 flex shrink items-center justify-center">
-					02
-				</div>
-				<div className="bg-indigo-600 rounded-lg text-white w-14 h-14 order-2 flex flex-none items-center justify-center">
-					03
-				</div>
+			<div className={itemClasses}>
+				<div className={`${shapeClasses} bg-indigo-600 flex order-2`}>01</div>
+				<div className={`${shapeClasses} bg-indigo-600 flex order-3`}>02</div>
+				<div className={`${shapeClasses} bg-indigo-600 flex order-1`}>03</div>
 			</div>
 		</div>
 	);
